7d047ee81c210b0ebd8205799ea63e2d3e275306
Cizaquita <cristian.izaquita@gmail.com>
2017-11-10 00:32:06 -0500

    Cambiado el acceso a la pagina principal, solicita login

--- reg_ciudadanos/reg_ciudadanos/settings.py
+++ reg_ciudadanos/reg_ciudadanos/settings.py
@@ -150,3 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Login redirect
+LOGIN_REDIRECT_URL='/'
+# Logout redirect
+LOGOUT_REDIRECT_URL='/'
